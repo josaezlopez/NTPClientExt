80704f4d32d4d3811d0ab9e178b99f7e35e128f9
josaezlopez <josaezlopez@gmail.com>
2024-10-01 15:42:13 +0200

    NTPClientExt.hpp

--- NTPClientExt.hpp
+++ NTPClientExt.hpp
@@ -1,254 +1,90 @@
-#include <NTPClientExt.hpp>
+#pragma once
 
+#include <Arduino.h>
+
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 #if defined(ESP8266)
-
-// Constructor
-NTPClientExt::NTPClientExt(const char* poolServerName, int timeOffset, int updateInterval,bool daylightSaving):
-      NTPClient(ntpUDP, poolServerName, timeOffset * 3600, updateInterval * 1000 * 60){
-      _daylightSaving = daylightSaving;
-      _timeZone = timeOffset;
-
-}
+  #include <ESP8266WiFi.h>
 
 #elif defined(ESP32)
+  #include <threadesp32.h>
 
-// Constructor
-NTPClientExt::NTPClientExt(const char* poolServerName, int timeOffset, int updateInterval,bool daylightSaving)
-  : NTPClient(ntpUDP, poolServerName, timeOffset * 3600, updateInterval * 1000 * 60), 
-    TaskParent(NTPTASK_NAME,NTPTASK_HEAP,NTPTASK_PRIORITY, NTPTASK_CORE){
-      _daylightSaving = daylightSaving;
-      _timeZone = timeOffset;
-
-}
+  #define NTPTASK_NAME        "NTP"
+  #define NTPTASK_HEAP        (3 * 1024)
+  #define NTPTASK_PRIORITY    TaskParent::th_LOWEST_PRIORITY
+  #define NTPTASK_CORE        1
 
 #endif
 
+#define NTP_MONTHENDSUMMERTIME 10
+#define LEAPYEAR(year)  (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? true : false)
 
+#if defined(ESP8266)
+  class NTPClientExt : public NTPClient{
 
-
-// Set the clock to daylight saving time.
-void NTPClientExt::setDaylightSaving(int startMonth,int endMonth){
-  _monthStartsSummerTime =   startMonth;
-  _monthEndsSummerTime = endMonth; 
-  _daylightSaving = true;
-  if(WiFi.status() == WL_CONNECTED) forceUpdate();
-}
-
-// Unset the clock to daylight saving time.
-void NTPClientExt::unsetDaylightSaving(){
-  _daylightSaving = false;
-  if(WiFi.status() == WL_CONNECTED) forceUpdate();  
-}
-
-// Returns a string with the date formatted as YYYY-MM-DD
-String NTPClientExt::getFormattedDate(){
-  getTS();
-  strftime(cBuffer, sizeof(cBuffer), "%Y-%m-%d", &ts);
-  buffer=String(cBuffer);
-  return buffer;
-}
-
-// Returns a char* with the date formatted as YYYY-MM-DD
-char* NTPClientExt::cGetFormattedDate(){
-  getTS();
-  strftime(cBuffer, sizeof(cBuffer), "%Y-%m-%d", &ts);
-  return cBuffer;
-}
-
-// Returns a string with the date formatted as YYYY-MM-DD HH:MM:SS
-String NTPClientExt::getFormattedDateTime(){
-  getTS();
-  strftime(cBuffer, sizeof(cBuffer), "%Y-%m-%d %H:%M:%S", &ts);
-  buffer=String(cBuffer);
-  return buffer;
-}
-
-// Returns a char* with the date formatted as YYYY-MM-DD HH:MM:SS
-char* NTPClientExt::cGetFormattedDateTime(){
-  getTS();
-  strftime(cBuffer, sizeof(cBuffer), "%Y-%m-%d %H:%M:%S", &ts);
-  return cBuffer;
-}
-
-// Returns a string with the time formatted as HH:MM
-String NTPClientExt::getTime(){
-  getTS();
-  strftime(cBuffer, sizeof(cBuffer), "%H:%M", &ts);
-  buffer=String(cBuffer);
-  return buffer;
-}
-
-// Returns a string with the time formatted as HH:MM:SS
-String NTPClientExt::getTimeS(){
-  getTS();
-  strftime(cBuffer, sizeof(cBuffer), "%H:%M:%S", &ts);
-  buffer=String(cBuffer);
-  return buffer;
-}
-
-// Returns a char* with the time formatted as HH:MM
-char* NTPClientExt::cGetTime(){
-  getTS();
-  strftime(cBuffer, sizeof(cBuffer), "%H:%M", &ts);
-  return cBuffer;
-}
-
-
-// Returns the year as an integer
-int  NTPClientExt::getYear(){
-  return getTS()->tm_year + 1900;
-}
-
-// Returns the month as an integer (1-12)
-int  NTPClientExt::getMonth(){
-  return getTS()->tm_mon + 1;
-}
-
-// Returns the number of the day of the month as an integer (1-31)
-int  NTPClientExt::getMDay(){
-  return getTS()->tm_mday;
-}
-
-// Returns the number of the day of the year (1-365 or 1-366 leaps)
-int  NTPClientExt::getDayOfTheYear(){
-  return getTS()->tm_yday;
-}
-
-
-#if defined(ESP32)
-
-/* Funcion de la tarea */
-void NTPClientExt::loop(){
-  if(!WiFi.isConnected()){
-    delay(1000);
-  }
-  
-  NTPClient::begin();
-  while(true){
-    if(WiFi.isConnected() ){
-      update();
-      }
-   delay(1000);
-  }
-}  
-
+#elif defined(ESP32)
+  class NTPClientExt : public NTPClient, public TaskParent{
 #endif
 
-
-
-// Update the time, if necessary
-void NTPClientExt::update(){
-  if(_daylightSaving){
-    if(itsSummerTime())
-      setTimeOffset((_timeZone + 1) * 3600);   
-    else
-      setTimeOffset(_timeZone  * 3600);      
-    }  
-  if(isConnected()){
-    NTPClient::update();  
-    }
-
-}
-
-
-
-// Returns and updates the ts structure
-struct tm* NTPClientExt::getTS(){
-  time_t epoch = getEpochTime();
-  ts = *localtime(&epoch);
-  return &ts;
-}
-
-// Return tm structure from an arbitrary epoch value
-struct tm* NTPClientExt::epoch2tm(time_t epoch){
-  tsTemp = *localtime(&epoch);
-  return &tsTemp;
-}
-
-
-// Returns the day of the week (0-6)
-// Perpetual calendar from 1700 to 2299
-// 0=Sunday, 1=Monday ... 6=Saturday
-int NTPClientExt::getWeekday(int year,int month, int day){
+  public:
   
-  int cCentury = _tcCentury[(year - 1700) / 100];
-  int cYear  = (year % 100) + (year % 100) / 4;
-  int cLeapYear = (LEAPYEAR(year) && (month==1 || month==2)) ? -1 : 0; 
-  int cMonth = _tcMonth[month - 1];
-  
-  return (cCentury + cYear + cLeapYear + cMonth + day) % 7;
+    /* Constructor */
+    NTPClientExt(const char* _poolServerName, int _timeOffset, int _updateInterval,bool _daylightSaving = false);
+    String getFormattedDate();      
+    char*  cGetFormattedDate();
 
-}
+    String getFormattedDateTime();
+    char*  cGetFormattedDateTime();
 
-// Returns the day of the week (0-6) of the current date
-// Perpetual calendar from 1700 to 2299
-// 0=Sunday, 1=Monday ... 6=Saturday
-int NTPClientExt::getWeekday(){
-  
-  int year = getYear();
-  int month = getMonth();
-  int day = getMDay();
+    String getTime();
+    String getTimeS();
+    char*  cGetTime();
 
-  int cCentury = _tcCentury[(year - 1700) / 100];
-  int cYear  = (year % 100) + (year % 100) / 4;
-  int cLeapYear = (LEAPYEAR(year) && (month==1 || month==2)) ? -1 : 0; 
-  int cMonth = _tcMonth[month - 1];
-  
-  return (cCentury + cYear + cLeapYear + cMonth + day) % 7;
+    int  getYear();
+    int  getMonth();
+    int  getMDay();
+    int  getWeekday(int anyo,int mes, int dia);
+    int  getWeekday();
+    int  getLastSundayOfMonth(int anyo,int mes);
+    bool itsSummerTime();
+    void update();
+    void setDaylightSaving(int _startMonth,int _endMonth);
+    void unsetDaylightSaving();
+    void setTimeZone(int tz);
+    int  getDayOfTheYear();
+    const char* getNameOfMonth(int day);
+    const char* getNameOfDay(int dia);
+    void setLang(const char* _lang);
+    struct tm* epoch2tm(time_t epoch);
 
-}
 
+  protected:
+    void loop();
+    bool isConnected() { return WiFi.status() == WL_CONNECTED; }
+    struct tm ts;
+    struct tm tsTemp;
+    String buffer;
+    char cBuffer[50];
+    struct tm* getTS();
+    WiFiUDP ntpUDP;
 
-// Returns the day of the last Sunday of the month (to calculate daylight saving time)
-int NTPClientExt::getLastSundayOfMonth(int year,int month){
-  int dayOfMonth = _daysOfTheMonth[month - 1];
-  if(LEAPYEAR(year) && month==2) 
-    dayOfMonth++;
-  return dayOfMonth - getWeekday(year,month,dayOfMonth);
-}
-
-// Returns true if we are in daylight saving time
-bool NTPClientExt::itsSummerTime(){
-  int year = getYear();
-  char frmt[] = "%u-%02u-%02u %02u:00:00";
-  char buffer[25];
-
-  sprintf(buffer,frmt,year,_monthStartsSummerTime,getLastSundayOfMonth(year,_monthStartsSummerTime),2);
-  String iniSunmmerTime = buffer;
-  sprintf(buffer,frmt,year,_monthEndsSummerTime,getLastSundayOfMonth(year,_monthEndsSummerTime),3);
-  String endSummerTime = buffer;
-  String now = getFormattedDateTime();
-
-  if(now > iniSunmmerTime && now < endSummerTime)
-    return true;
-  else
-    return false;
-}
-
-// Returns the name of the month as char*
-const char* NTPClientExt::getNameOfMonth(int _month){
-  return _nombreMeses[_lang][_month];
-}
-
-const char* NTPClientExt::getNameOfDay(int dia){ 
-  return _nombreDia[_lang][dia]; 
-  }
-
-// Set the language 0 = Spanish, 1 = English
-void NTPClientExt::setLang(const char* lang){
-  _lang = 0;
-  if(strcmp(lang,"ES")==0)
-    _lang = 0;
-  if(strcmp(lang,"EN")==0)
-    _lang = 1;
-
-}
-
-// Set the utc time zone
-void NTPClientExt::setTimeZone(int tz){
-  _timeZone = tz;
-  setTimeOffset(tz * 3600);
-  if(WiFi.status() == WL_CONNECTED) forceUpdate();
     
-}
+  private:
+    int _timeZone=0;
+    int _tcCentury[6] = {5,3,1,0,-2,-4};
+    int _tcMonth[12]  = {6,2,2,5,0,3,5,1,4,6,2,4};
+    int _daysOfTheMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int _monthStartsSummerTime =   3;  // The last Sunday of March (Western Europe)
+    int _monthEndsSummerTime = 10;     // The last Sunday in October (Western Europe)
+    bool _daylightSaving;
+    char _nombreMeses[2][12][5] = {" Ene"," Feb"," Mar"," Abr"," May"," Jun"," Jul"," Ago"," Sep"," Oct"," Nov"," Dic",
+                                  " Jan"," Feb"," Mar"," Apr"," May"," Jun"," Jul"," Aug"," Sep"," Oct"," Nov"," Dec"};
+    char _nombreDia[2][7][10] = {"Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado",
+                               "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    int _lang = 1;
+
+};                              
+
+
